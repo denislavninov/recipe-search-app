fd134bcea671c53196998153d6ac2e56c7e9a8d9
denislavninov <deniztopcuoglu@gmail.com>
2024-09-28 00:38:52 +0200

    add Lesson36-assignment

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,11 +1,15 @@
 import { RecipeList } from "./modules/recipes/RecipeList";
 import { SearchBar } from "./modules/recipes/search-bar";
-import { fetchRecipesByIngredient } from "./modules/recipes/recipeService";
+import {
+  fetchRecipesByIngredient,
+  fetchRecipesById,
+} from "./modules/recipes/recipeService";
 import "./App.css";
 import { useState, useEffect } from "react";
 
 export const App = () => {
   const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, SetSelectedRecipe] = useState(null); // to store selected recipe details
 
   useEffect(() => {
     fetchRecipesByIngredient("chicken_breast").then((recipes) =>
@@ -13,11 +17,44 @@ export const App = () => {
     );
   }, []);
 
+  const handleRecipeClick = (idMeal) => {
+    fetchRecipesById(idMeal)
+      .then((recipeDetails) => {
+        console.log("Fetched recipe details:", recipeDetails);
+        console.log("Recipe ID to fetch details:", idMeal);
+        SetSelectedRecipe(recipeDetails); // store recipe details in state
+      })
+      .catch((error) => console.error("Error fetching recipe details:", error));
+  };
+
   return (
     <div className="container">
       <header>Recipe Search App</header>
       <SearchBar />
-      <RecipeList recipes={recipes} />
+      {selectedRecipe ? (
+        <div className="recipe-details">
+          <h2>{selectedRecipe.strMeal}</h2>
+          <img
+            src={selectedRecipe.strMealThumb}
+            alt={selectedRecipe.strMeal}
+          ></img>
+          <p>{selectedRecipe.strInstructions}</p>
+          <ul>
+            {Object.keys(selectedRecipe)
+              .filter(
+                (key) => key.startsWith("strIngredient") && selectedRecipe[key],
+              )
+              .map((key) => (
+                <li key={key}>{selectedRecipe[key]}</li>
+              ))}
+          </ul>
+          <button onClick={() => SetSelectedRecipe(null)}>
+            Back to Recipe List
+          </button>
+        </div>
+      ) : (
+        <RecipeList recipes={recipes} onRecipeClick={handleRecipeClick} />
+      )}
     </div>
   );
 };
--- recipe-app/src/modules/recipes/recipeService.js
+++ recipe-app/src/modules/recipes/recipeService.js
@@ -1,10 +1,22 @@
-export async function fetchRecipesByIngredient (query) {
+export async function fetchRecipesByIngredient(query) {
   const response = await fetch(
     `https://www.themealdb.com/api/json/v1/1/filter.php?i=${query}`,
   );
   if (!response.ok) {
-    throw Error('An error occured when fetching data.')
+    throw Error("An error occured when fetching data.");
   }
   const data = await response.json();
   return data.meals;
+}
+
+export async function fetchRecipesById(idMeal) {
+  const response = await fetch(
+    `https:/www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`,
+  );
+  if (!response.ok) {
+    throw Error("An error occured when fetching data.");
+  }
+
+  const data = await response.json();
+  return data.meals[0];
 }
