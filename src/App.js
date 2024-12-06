8d4187134bb1be45ba91c10ef69cb937d15ae78b
denislavninov <deniztopcuoglu@gmail.com>
2024-12-06 15:09:14 +0100

    added lesson41 Mui styles

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from "react";
 import { RecipesProvider } from "./modules/recipes/RecipesProvider";
 import "./App.css";
 import { AppRouter } from "./AppRouter";
-import { Navbar } from "./modules/NavBar/NavbarComponent";
 import { UserProvider } from "./modules/user/UserContext";
+import Container from "@mui/material/Container";
+import { NavBar } from "./shared-component/navbar";
 
 export const App = () => {
   return (
     <UserProvider>
       <RecipesProvider initialState={[]}>
         <>
-          <Navbar />
-          <div className="container">
+          <NavBar />
+
+          <Container maxWidth="xl" sx={{ mt: 4 }}>
             <AppRouter />
-          </div>
+          </Container>
         </>
       </RecipesProvider>
     </UserProvider>
