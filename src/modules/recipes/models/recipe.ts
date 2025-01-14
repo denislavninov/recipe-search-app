6f601643c0cb1f902de272c56cc7fbe21a02544f
denislavninov <deniztopcuoglu@gmail.com>
2025-01-14 22:50:14 +0100

    added L43-ts-all-info

--- src/modules/recipes/models/recipe.ts
+++ src/modules/recipes/models/recipe.ts
@@ -3,12 +3,32 @@ export interface Recipe {
   strMeal: string;
   strMealThumb: string;
   strInstructions: string;
-  strCategory?: string; // Example additional property
-  strArea?: string; // Example additional property
-  strTags?: string; // Example additional property
-  strYoutube?: string; // Example additional property
-  strIngredient1?: string; // Example additional property
-  strIngredient2?: string; // Example additional property
-  // Add all other properties from the API response here
-  [key: string]: any; // For other dynamic properties like ingredients
-} 
+  strCategory?: string;
+  strArea?: string;
+  strTags?: string;
+  strYoutube?: string;
+  strIngredient1?: string;
+  strIngredient2?: string;
+  strIngredient3?: string;
+  strIngredient4?: string;
+  strIngredient5?: string;
+  strIngredient6?: string;
+  strIngredient7?: string;
+  strIngredient8?: string;
+  strIngredient9?: string;
+  strIngredient10?: string;
+  strIngredient11?: string;
+  strIngredient12?: string;
+  strIngredient13?: string;
+  strIngredient14?: string;
+  strIngredient15?: string;
+  strIngredient16?: string;
+  strIngredient17?: string;
+  strIngredient18?: string;
+  strIngredient19?: string;
+  strIngredient20?: string;
+  strSource?: string;
+  strCreativeCommonsConfirmed?: string;
+  dateModified?: string;
+  [key: string]: any;
+}
